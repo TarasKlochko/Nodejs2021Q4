31aed20eae917e1872f5cf8d623bbc1a1283a82a
TarasKlochko <60792921+TarasKlochko@users.noreply.github.com>
2021-11-19 12:56:06 +0200

    init: satrt Task 2 Testing

---
+++ Task2_Testing/transforms.js
@@ -0,0 +1,28 @@
+import { Transform } from 'stream';
+import { cipher } from './cipher.js';
+import { optionsValue } from './options.js';
+
+export let transforms = [];
+
+function createTransformStream(shift, encrypt, atbash = 0) {
+  const transform = new Transform({
+    transform(chunk, encoding, callback) {
+      const chankStringigied = chunk.toString().trim();
+      callback(null, cipher(chankStringigied, shift, encrypt, atbash) + '\n');
+    },
+  });
+  transforms.push(transform);
+}
+
+optionsValue.configValue.split('-').forEach((config) => {
+  let [cipherType, value] = config.split('');
+  if (cipherType === 'C') {
+    createTransformStream(1, +value, 0);
+  }
+  if (cipherType === 'R') {
+    createTransformStream(8, +value, 0);
+  }
+  if (cipherType === 'A') {
+    createTransformStream(null, null, 1);
+  }
+});
