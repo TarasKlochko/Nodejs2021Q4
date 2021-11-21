e29447d3e74624990330e6ebddf46de9042692fc
TarasKlochko <60792921+TarasKlochko@users.noreply.github.com>
2021-11-21 18:53:10 +0200

    fix: delete extra code

--- Task1_Ciphering-CLI-Tool/input.js
+++
@@ -1,4 +0,0 @@
-import fs from 'fs';
-import { optionsValue } from './options.js';
-
-export const input = optionsValue.inputValue ? fs.createReadStream(optionsValue.inputValue, 'utf-8') : process.stdin;
--- Task1_Ciphering-CLI-Tool/output.js
+++
@@ -1,6 +0,0 @@
-import fs from 'fs';
-import { optionsValue } from './options.js';
-
-export const output = optionsValue.outputValue
-  ? fs.createWriteStream(optionsValue.outputValue, { flags: 'a' })
-  : process.stdout;
